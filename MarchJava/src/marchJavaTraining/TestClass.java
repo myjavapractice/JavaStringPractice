3f32348ca08ed6449c79564ebb94bcae9f3bc7f9
myjavapractice <nbaisetti@gmail.com>
2018-04-02 17:10:51 -0400

    added test

--- MarchJava/src/marchJavaTraining/TestClass.java
+++ MarchJava/src/marchJavaTraining/TestClass.java
@@ -3,7 +3,7 @@ package marchJavaTraining;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
+		System.out.println("Test");//added a comment
 	}
 
 }
