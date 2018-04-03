2c2b2399d07542ce2e29111408b442d3c68186d2
myjavapractice <nbaisetti@gmail.com>
2018-04-03 11:36:19 -0400

    changes

---
+++ JavaPracticeExamples/src/examplesToInvokeFromOtherPackages/InvokeStringCheck.java
@@ -0,0 +1,12 @@
+package examplesToInvokeFromOtherPackages;
+
+
+public class InvokeStringCheck {
+
+	public static void main(String[] args) {
+		//StringCheck sc = new StringCheck();
+		//sc.containsCheck();
+
+	}
+
+}
